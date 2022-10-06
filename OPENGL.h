7c37d0e2ce91a2e5147f498c445397b792f73379
梦呓qwq <2934553744@qq.com>
2022-10-06 12:15:37 +0800

    first commit

---
+++ OPENGL.h
@@ -0,0 +1,14 @@
+#ifndef OPENGLCAA_H
+#define OPENGLCAA_H
+#define STB_IMAGE_IMPLEMENTATION
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Header/stb_image.h"
+#include <bits/stdc++.h>
+#include "glad.c"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+using namespace std;
+#endif
